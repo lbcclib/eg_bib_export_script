3a15fb622fa90ab2c7073d2c4e5127caa6663537
Jane Sandberg <sandbergja@users.noreply.github.com>
2015-09-03 14:21:49 -0700

    adding logging and email parts

--- export_lbcc_biblio_records.sh
+++ export_lbcc_biblio_records.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-dbname="db"
-username="user"
+dbname="db" #change this to appropriate value
+username="user" #change this to appropriate value
+email="sandbej at linnbenton dot edu" #change this to appropriate value
+eg_version="2.7.4" #change this when we upgrade Evergreen
+
 today=$(date +"%Y-%m-%d")
 starting_dir=$(pwd)
 filename="lbcc_marc_records.$today.mrc"
+log_file="lbcc_marc_records.$today.log"
 
 
 psql $dbname $username << EOF
@@ -18,10 +22,11 @@ SELECT  DISTINCT b.id as tcn
   AND (c.circ_lib=7
     AND c.deleted=FALSE)
   OR xpath_exists('//m:datafield[@tag="856"]/m:subfield[@code="9" and text()="LBCC"]', b.marc::xml, ARRAY[ARRAY['m','http://www.loc.gov/MARC21/slim']]);
-EOF
+EOF > $log_file
 
 tail -n +2 lbcc_tcns_to_export.old > lbcc_tcns_to_export
 rm lbcc_tcns_to_export.old
-cd /home/opensrf/Evergreen-ILS-2.7.4/Open-ILS/src/support-scripts/
-cat $starting_dir/lbcc_tcns_to_export | ./marc_export -i -c /openils/conf/opensrf_core.xml -x /openils/conf/fm_IDL.xml -f USMARC --timeout 5 > $starting_dir/eg_records_lbcc_findit.$today.mrc
+cd /home/opensrf/Evergreen-ILS-$eg_version/Open-ILS/src/support-scripts/ 2>> $starting_dir/$log_file
+cat $starting_dir/lbcc_tcns_to_export | ./marc_export -i -c /openils/conf/opensrf_core.xml -x /openils/conf/fm_IDL.xml -f USMARC --timeout 5 > $starting_dir/eg_records_lbcc_findit.$today.mrc 2>> $starting_dir/$log_file
 rm $starting_dir/lbcc_tcns_to_export
+cat $starting_dir/$log_file | mail -s "Completed discovery layer export" $email
