d60c02914eeb358c0817621bcef0a0f3b683c188
Jane Sandberg <sandbergja@gmail.com>
2022-01-19 14:37:33 -0800

    Send records to Ebsco via FTP; incorporate updates from Jeremy

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # eg_bib_export_script
-This is a quick BASH script written with the guidance of the official [Evergreen documentation for the marc_export support script](http://docs.evergreen-ils.org/2.7/_support_scripts.html#_marc_export_exporting_bibliographic_records_into_marc_files).
+This is a quick BASH script written with the guidance of the official [Evergreen documentation for the marc_export support script](https://docs.evergreen-ils.org/eg/docs/latest/development/support_scripts.html#marc_export).
+
+It exports bib records, then FTPs them to our discovery layer (EDS).
--- export_lbcc_biblio_records.sh
+++ export_lbcc_biblio_records.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 
-dbname="db" #change this to appropriate value
-username="user" #change this to appropriate value
-email="sandbej at linnbenton dot edu" #change this to appropriate value
-eg_version="2.9.1" #change this when we upgrade Evergreen
-
+email="[REDACTED]"
+target_dir='/openils/var/web/filepile/discovery_layer_exports'
 today=$(date +"%Y-%m-%d")
-starting_dir=$(pwd)
 filename="lbcc_marc_records.$today.mrc"
 log_file="lbcc_marc_records.$today.log"
+host="ftp.epnet.com"
+user="[REDACTED]"
+password='[REDACTED]'
+remote_directory='full'
 
 
-psql $dbname $username > $log_file << EOF
-\o lbcc_tcns_to_export.old
-  SELECT DISTINCT b.id as tcn
-  FROM biblio.record_entry b
-  INNER JOIN asset.call_number cn ON cn.record=b.id
-  WHERE b.deleted=FALSE
-  AND b.id != -1
-  AND (cn.owning_lib=7
-  AND cn.deleted=FALSE);
-EOF
+/openils/bin/marc_export --descendants LINN --items -f USMARC -e UTF-8 >  $target_dir/$filename 2>> $log_file
+/openils/bin/marc_export --descendants LBCC --uris  -f USMARC -e UTF-8 >> $target_dir/$filename 2>> $log_file
 
-tail -n +2 lbcc_tcns_to_export.old > lbcc_tcns_to_export
-rm lbcc_tcns_to_export.old
-cd /home/opensrf/Evergreen-ILS-$eg_version/Open-ILS/src/support-scripts/ 2>> $starting_dir/$log_file
-cat $starting_dir/lbcc_tcns_to_export | ./marc_export -i -c /openils/conf/opensrf_core.xml -x /openils/conf/fm_IDL.xml -f USMARC -e UTF-8 --timeout 5 > $starting_dir/eg_records_lbcc_findit.$today.mrc 2>> $starting_dir/$log_file
-rm $starting_dir/lbcc_tcns_to_export
-cat $starting_dir/$log_file | mail -s "Completed discovery layer export" $email
+# FTP the records to the remote server
+ftp -n $host <<END_FTP
+quote USER $user
+quote PASS $password
+binary
+cd $remote_directory
+put $target_dir/$filename
+quit
+END_FTP
+
+# For testing, perhaps you'd prefer to read some XML rather than binary MARC?
+#/openils/bin/marc_export --descendants LINN --items -f XML -e UTF-8 >  $target_dir/$filename 2>> $log_file
+#/openils/bin/marc_export --descendants LBCC --uris  -f XML -e UTF-8 >> $target_dir/$filename 2>> $log_file
+
+cat $log_file | mail -s "Completed discovery layer export" $email
+
+#rm $log_file
+# For testing, get a quick count of how many records were exported using the XML export format
+#grep "controlfield tag=\"001\"" $target_dir/$filename | wc -l
